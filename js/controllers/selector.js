5c3852a8754c36c2c1a209ed6edeb2e7fc6dbc26
Pablo Dávila <a00513157@itesm.mx>
2019-12-07 17:55:26 -0600

    corrected js import in nonlineal.html

--- js/controllers/selector.js
+++ js/controllers/selector.js
@@ -6,24 +6,26 @@ selector = () => {
             console.log("NonLinear");
             window.location.href = "nonlineal.html";
             break;
+
         case 2:
             console.log("Polynomial");
             window.location.href = "polynomial.html";
             break;
+
         case 3:
             console.log("linealSystem");
             window.location.href = "lineal_system.html";
-
             break;
+
         case 4:
             console.log("NonLinearSystem");
             window.location.href = "non_lineal_system.html";
-
-
             break;
+
         case 5:
             console.log("ordDifEquation");
             window.location.href = "ord_diferential.html";
             break;
+
     }
 };
